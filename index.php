005c259fa187b5c6f14b8672a95d12aa89cca072
Merijn <99054535@mydavinci.nl>
2020-02-11 14:47:17 +0100

    tekst font en kleur klaar

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="dag.css">
+    <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
     <title>Goedendag</title>
 <style>
     *{
@@ -35,6 +35,7 @@ body {
     background-size: cover;
 }
 h1{
+    font-family: 'Lobster', cursive;
     text-align:center;
     color:white;
 }
