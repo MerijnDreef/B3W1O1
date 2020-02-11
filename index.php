bbde4115e31991566e59bb8c143ca06d526c3120
Merijn <99054535@mydavinci.nl>
2020-02-11 14:45:19 +0100

    bijna klaar
    :)

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
 <?php 
-    $time = strtotime('+1 hour');
-    strtotime('+1 hour', $time);
-    $time = date("H:i", strtotime('+0 hour'));
-    // if(){
-
-    // }
+    $time = date("H");
+    if($time >= "6" && $time <= "12"){
+        $txt = "Goede morgen!";
+        $img = "morning.png";
+    }
+    else if($time >= "12" && $time <= "18"){
+        $txt = "Goede middag!";
+        $img = "afternoon.png";
+    }
+    else if($time >= "18" && $time <= "0"){
+        $txt = "Goede avond!";
+        $img = "evening.png";
+    }
+    else if($time >= "0" && $time <= "6"){
+        $txt = "Goede nacht!";
+        $img = "night.png";
+    }
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -13,12 +24,27 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="dag.css">
     <title>Goedendag</title>
+<style>
+    *{
+    margin:0;
+    padding:0;
+}
+
+body {
+    background-image: url("<?= $img ?>");
+    background-size: cover;
+}
+h1{
+    text-align:center;
+    color:white;
+}
+</style>
 </head>
 <body>
-    <img src="">
     <h1>
         <?php
-         echo "Het is nu " . $time;
+         echo $txt . "<br>";
+         echo "Het is nu " . date("H:i");
         ?></h1>
 </body>
 </html>
